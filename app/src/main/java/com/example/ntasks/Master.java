6e8b53770b920e874dd5f02806ad60fc271ab010
sulemanahmed-cyber <mohdsuleman2626@gmail.com>
2024-01-03 11:39:07 +0530

    dimens and 02jan

--- app/src/main/java/com/example/ntasks/Master.java
+++ app/src/main/java/com/example/ntasks/Master.java
@@ -1,29 +1,25 @@
 package com.example.ntasks;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
 import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class Master extends AppCompatActivity {
-Button Testify;
+    Button Testify;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_master);
-        Button thingstd=findViewById(R.id.thingstd);
-
+        Button thingstd = findViewById(R.id.thingstd);
+        Button clientbtn = findViewById(R.id.clientbtn);
         // Get the ActionBar
 
 
-        Testify=findViewById(R.id.testify);
-
-
+        Testify = findViewById(R.id.testify);
 
 
         if (getSupportActionBar() != null) {
@@ -31,18 +27,29 @@ Button Testify;
             getSupportActionBar().hide();
 
         }
-Testify.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-        ////Intent intent2 =new Intent(Master.this, testify.class);
-      ///  startActivity(intent2);
-    }
-});
+        Testify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ////Intent intent2 =new Intent(Master.this, testify.class);
+                ///  startActivity(intent2);
+            }
+        });
+
+        clientbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent2 =new Intent(Master.this,AddClientActivity.class);
+                startActivity(intent2);
+            }
+        });
+
+
 
         thingstd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent= new Intent(Master.this, MainActivity.class);
+                Intent intent = new Intent(Master.this, MainActivity.class);
                 startActivity(intent);
             }
         });
