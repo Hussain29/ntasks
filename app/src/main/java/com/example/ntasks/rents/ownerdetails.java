3ec54c6353342775b0fef2ea0adc16b20515c4f1
sulemanahmed-cyber <mohdsuleman2626@gmail.com>
2024-01-09 19:18:35 +0530

    folderchange

--- app/src/main/java/com/example/ntasks/rents/ownerdetails.java
+++ app/src/main/java/com/example/ntasks/rents/ownerdetails.java
@@ -1,8 +1,10 @@
-package com.example.ntasks;
+package com.example.ntasks.rents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.ntasks.R;
 
 public class ownerdetails extends AppCompatActivity {
 
