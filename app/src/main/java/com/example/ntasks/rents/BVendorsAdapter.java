6e0ff54d41d33408358025df6982af06133f8dd7
sulemanahmed-cyber <mohdsuleman2626@gmail.com>
2024-01-17 18:42:17 +0530

    intents and searchbar

--- app/src/main/java/com/example/ntasks/rents/BVendorsAdapter.java
+++ app/src/main/java/com/example/ntasks/rents/BVendorsAdapter.java
@@ -1,9 +1,12 @@
 package com.example.ntasks.rents;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -14,11 +17,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.ntasks.R;
 
 import java.util.ArrayList;
-
-public class BVendorsAdapter extends RecyclerView.Adapter<BVendorsAdapter.BVendorsViewHolder> {
+public class BVendorsAdapter extends RecyclerView.Adapter<BVendorsAdapter.BVendorsViewHolder> implements Filterable {
 
     private Context context;
     private ArrayList<BusinessVendor> vendorList;
+    private ArrayList<BusinessVendor> filteredList;
     private OnItemClickListener mListener;
 
     public interface OnItemClickListener {
@@ -32,6 +35,7 @@ public class BVendorsAdapter extends RecyclerView.Adapter<BVendorsAdapter.BVendo
     public BVendorsAdapter(Context context, ArrayList<BusinessVendor> vendorList) {
         this.context = context;
         this.vendorList = vendorList;
+        this.filteredList = new ArrayList<>(vendorList);
     }
 
     @NonNull
@@ -43,7 +47,7 @@ public class BVendorsAdapter extends RecyclerView.Adapter<BVendorsAdapter.BVendo
 
     @Override
     public void onBindViewHolder(@NonNull BVendorsViewHolder holder, int position) {
-        BusinessVendor vendor = vendorList.get(position);
+        BusinessVendor vendor = filteredList.get(position);
 
         holder.tvVendorName.setText(vendor.getVendorName());
         holder.tvVendorProducts.setText(vendor.getProducts());
@@ -54,7 +58,7 @@ public class BVendorsAdapter extends RecyclerView.Adapter<BVendorsAdapter.BVendo
             public void onClick(View v) {
                 int adapterPosition = holder.getAdapterPosition();
                 if (mListener != null && adapterPosition != RecyclerView.NO_POSITION) {
-                    mListener.onItemClick(vendorList.get(adapterPosition));
+                    mListener.onItemClick(filteredList.get(adapterPosition));
                 }
             }
         });
@@ -62,7 +66,45 @@ public class BVendorsAdapter extends RecyclerView.Adapter<BVendorsAdapter.BVendo
 
     @Override
     public int getItemCount() {
-        return vendorList.size();
+        return filteredList.size();
+    }
+
+    public void setDataList(ArrayList<BusinessVendor> dataList) {
+        this.vendorList = dataList;
+        this.filteredList = new ArrayList<>(dataList);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String filterPattern = charSequence.toString().toLowerCase().trim();
+
+                if (TextUtils.isEmpty(filterPattern)) {
+                    filteredList.clear();
+                    filteredList.addAll(vendorList);
+                } else {
+                    filteredList.clear();
+                    for (BusinessVendor vendor : vendorList) {
+                        if (vendor.getContactPerson().toLowerCase().contains(filterPattern) ||vendor.getVendorName().toLowerCase().contains(filterPattern) ||
+                                vendor.getMobileNumber().contains(filterPattern)||vendor.getProducts().toLowerCase().contains(filterPattern)) {
+                            filteredList.add(vendor);
+                        }
+                    }
+                }
+
+                FilterResults results = new FilterResults();
+                results.values = filteredList;
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                notifyDataSetChanged();
+            }
+        };
     }
 
     public static class BVendorsViewHolder extends RecyclerView.ViewHolder {
--- app/src/main/java/com/example/ntasks/rents/MasterBiVendors.java
+++ app/src/main/java/com/example/ntasks/rents/MasterBiVendors.java
@@ -1,12 +1,10 @@
 package com.example.ntasks.rents;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
@@ -14,14 +12,13 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.SearchView;
 import com.example.ntasks.R;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
 
 public class MasterBiVendors extends AppCompatActivity {
@@ -30,6 +27,8 @@ public class MasterBiVendors extends AppCompatActivity {
     private BVendorsAdapter bVendorsAdapter;
     private ArrayList<BusinessVendor> vendorList;
     private ProgressDialog progressDialog;
+    private SearchView searchView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +36,7 @@ public class MasterBiVendors extends AppCompatActivity {
         setContentView(R.layout.activity_master_bi_vendors);
 
         Button btnAddVendor = findViewById(R.id.btnaddbven);
+        searchView = findViewById(R.id.searchview);
 
         btnAddVendor.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,7 +57,7 @@ public class MasterBiVendors extends AppCompatActivity {
         vendorList = new ArrayList<>();
 
         // Retrieve data from Firebase Realtime Database
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Rents/Vendors");
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("BusinessVendors");
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -66,7 +66,7 @@ public class MasterBiVendors extends AppCompatActivity {
                     BusinessVendor vendor = snapshot.getValue(BusinessVendor.class);
                     vendorList.add(vendor);
                 }
-                bVendorsAdapter.notifyDataSetChanged();
+                bVendorsAdapter.setDataList(vendorList); // Update the data
                 progressDialog.dismiss(); // Notify the adapter that the data has changed
             }
 
@@ -86,6 +86,21 @@ public class MasterBiVendors extends AppCompatActivity {
                 // Handle item click here
                 // You can open BusinessVendorDetailsActivity and pass relevant data through Intent
                 openBusinessVendorDetailsActivity(vendor);
+            }
+        });
+
+        // Set up search functionality
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Filter the data based on the search query
+                bVendorsAdapter.getFilter().filter(newText);
+                return true;
             }
         });
 
