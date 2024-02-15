83ddd6a795f6e0d5a18c8c99934fc2801fbf2b8e
sulemanahmed-cyber <mohdsuleman2626@gmail.com>
2024-02-15 16:11:32 +0530

    Edit acttivites

---
+++ app/src/main/java/com/example/ntasks/rents/EditVendorDetails.java
@@ -0,0 +1,46 @@
+package com.example.ntasks.rents;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.ntasks.R;
+
+public class EditVendorDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_vendor_details);
+
+        // Get the ActionBar
+        ActionBar actionBar = getSupportActionBar();
+
+        // Set the title
+        actionBar.setTitle("EDIT VENDORS");
+
+        // Enable the back button
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        int actionBarColor = ContextCompat.getColor(this, R.color.pinkkk);
+        actionBar.setBackgroundDrawable(new ColorDrawable(actionBarColor));
+
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
